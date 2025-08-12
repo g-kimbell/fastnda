c7ee402104c32fbe305b315b16fe14457a1ab5ab
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-08-12 16:58:05 +0200

    Add function to convert BTSDA CSVs to test files

--- fastnda/__init__.py
+++ fastnda/__init__.py
@@ -1,9 +1,11 @@
 """Public API."""
 
+from fastnda.btsda import btsda_csv_to_parquet
 from fastnda.main import read
 from fastnda.version import __version__
 
 __all__ = [
     "__version__",
+    "btsda_csv_to_parquet",
     "read",
 ]
