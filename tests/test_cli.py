421f9eba51d4b90c027fa5aaa2ea6e27caf9099b
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-12-04 11:58:22 +0100

    Add more CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,12 @@
 """Tests for fastnda CLI with optional dependencies."""
 
+import logging
+import shutil
 from pathlib import Path
 
 import pandas as pd
 import polars as pl
+import pytest
 from polars.testing import assert_frame_equal
 from typer.testing import CliRunner
 
@@ -28,7 +31,7 @@ class TestCliWithOptionalDeps:
                 "convert",
                 str(self.test_file),
                 str(output),
-                "--filetype=hdf5",
+                "--filetype=h5",
             ],
         )
         assert result.exit_code == 0
@@ -106,3 +109,191 @@ class TestCliWithOptionalDeps:
         assert output.exists()
         df = pl.read_parquet(output)
         assert_frame_equal(df, self.ref_df)
+
+    def test_convert_arrow(self, tmp_path: Path) -> None:
+        """Converting polars-style arrow."""
+        output = tmp_path / self.test_file.with_suffix(".arrow").name
+        result = self.runner.invoke(
+            app,
+            [
+                "convert",
+                str(self.test_file),
+                str(output),
+                "--filetype=arrow",
+            ],
+        )
+        assert result.exit_code == 0
+        assert output.exists()
+        df = pl.read_ipc(output)
+        assert_frame_equal(df, self.ref_df)
+
+    def test_auto_output(self, tmp_path: Path) -> None:
+        """Converting polars-style parquet."""
+        # copy file to tmp path
+        copied_file = tmp_path / self.test_file.name
+        shutil.copy(self.test_file, copied_file)
+        output = tmp_path / self.test_file.with_suffix(".parquet").name
+        result = self.runner.invoke(
+            app,
+            [
+                "convert",
+                str(copied_file),
+                "--filetype=parquet",
+            ],
+        )
+        assert result.exit_code == 0
+        assert output.exists()
+        df = pl.read_parquet(output)
+        assert_frame_equal(df, self.ref_df)
+
+    def test_empty_batch_convert(self, tmp_path: Path) -> None:
+        """Converting polars-style parquet."""
+        result = self.runner.invoke(
+            app,
+            [
+                "batch-convert",
+                str(tmp_path),
+            ],
+        )
+        assert result.exit_code == 1
+        assert "No .nda or .ndax files found." in str(result.exception)
+
+    def test_batch_convert(self, tmp_path: Path) -> None:
+        """Converting polars-style parquet."""
+        # copy file to tmp path
+        copied_file_1 = tmp_path / (self.test_file.stem + "_1.ndax")
+        copied_file_2 = tmp_path / (self.test_file.stem + "_2.ndax")
+        shutil.copy(self.test_file, copied_file_1)
+        shutil.copy(self.test_file, copied_file_2)
+        output_1 = copied_file_1.with_suffix(".parquet")
+        output_2 = copied_file_2.with_suffix(".parquet")
+        result = self.runner.invoke(
+            app,
+            [
+                "batch-convert",
+                str(tmp_path),
+                "--filetype=parquet",
+            ],
+        )
+        assert result.exit_code == 0
+        assert output_1.exists()
+        assert output_2.exists()
+        df = pl.read_parquet(output_1)
+        assert_frame_equal(df, self.ref_df)
+
+    def test_recursive_batch_convert(self, tmp_path: Path) -> None:
+        """Converting polars-style parquet."""
+        (tmp_path / "subfolder").mkdir()
+        copied_file_1 = tmp_path / "subfolder" / (self.test_file.stem + "_1.ndax")
+        shutil.copy(self.test_file, copied_file_1)
+        output_1 = copied_file_1.with_suffix(".parquet")
+        result = self.runner.invoke(
+            app,
+            [
+                "batch-convert",
+                str(tmp_path),
+                "--filetype=parquet",
+            ],
+        )
+        assert result.exit_code == 1
+        assert not output_1.exists()
+        assert "--recursive" in str(result.exception)
+
+        result = self.runner.invoke(
+            app,
+            ["batch-convert", str(tmp_path), "--filetype=parquet", "--recursive"],
+        )
+        assert result.exit_code == 0
+        assert output_1.exists()
+
+    def test_hdf5_batch_convert(self, tmp_path: Path) -> None:
+        """Batch convert hdf5 files."""
+        # copy file to tmp path
+        copied_file_1 = tmp_path / (self.test_file.stem + "_1.ndax")
+        copied_file_2 = tmp_path / (self.test_file.stem + "_2.ndax")
+        shutil.copy(self.test_file, copied_file_1)
+        shutil.copy(self.test_file, copied_file_2)
+        output_1 = copied_file_1.with_suffix(".h5")
+        output_2 = copied_file_2.with_suffix(".h5")
+        result = self.runner.invoke(
+            app,
+            [
+                "batch-convert",
+                str(tmp_path),
+                "--filetype=h5",
+            ],
+        )
+        assert result.exit_code == 0
+        assert output_1.exists()
+        assert output_2.exists()
+        df = pd.read_hdf(output_1, key="data")
+        assert_frame_equal(
+            pl.DataFrame(pl.from_pandas(df)),
+            self.ref_df.with_columns(pl.col("step_type").cast(pl.Categorical)),
+        )
+
+    def test_batch_convert_bad_inputs(self, tmp_path: Path) -> None:
+        """Test batch convert with bad inputs."""
+        result = self.runner.invoke(
+            app,
+            [
+                "batch-convert",
+                str(self.test_file),
+            ],
+        )
+        assert result.exit_code == 1
+        assert "not a folder" in str(result.exception)
+
+        result = self.runner.invoke(
+            app,
+            [
+                "batch-convert",
+                str(tmp_path / "subfolder" / "doesntexist"),
+            ],
+        )
+        assert result.exit_code == 1
+        assert "does not exist" in str(result.exception)
+
+    def test_batch_convert_bad_files(self, tmp_path: Path, caplog: pytest.FixtureRequest) -> None:
+        """Test that batch convert works even if there is a bad file."""
+        copied_file_1 = tmp_path / (self.test_file.stem + "_1.nda")
+        copied_file_2 = tmp_path / (self.test_file.stem + "_2.ndax")
+        # just make some file
+        with copied_file_1.open("w") as f:
+            f.write("this is not a real ndax file")
+        shutil.copy(self.test_file, copied_file_2)
+        output_1 = copied_file_1.with_suffix(".parquet")
+        output_2 = copied_file_2.with_suffix(".parquet")
+        result = self.runner.invoke(
+            app,
+            [
+                "batch-convert",
+                str(tmp_path),
+                "--filetype=parquet",
+            ],
+        )
+        assert result.exit_code == 0
+        assert not output_1.exists()
+        assert output_2.exists()
+
+    def test_verbosity(self, tmp_path: Path, caplog: pytest.FixtureRequest) -> None:  # noqa: ARG002
+        """Test batch convert with bad inputs."""
+        output = tmp_path / self.test_file.with_suffix(".parquet").name
+
+        self.runner.invoke(app, ["-vv", "convert", str(self.test_file), str(output)])
+        assert logging.getLogger().level == logging.DEBUG
+
+        self.runner.invoke(app, ["-v", "convert", str(self.test_file), str(output)])
+        assert logging.getLogger().level == logging.INFO
+
+        self.runner.invoke(app, ["convert", str(self.test_file), str(output)])
+        assert logging.getLogger().level == logging.WARNING
+
+        self.runner.invoke(app, ["-q", "convert", str(self.test_file), str(output)])
+        assert logging.getLogger().level == logging.CRITICAL
+
+        self.runner.invoke(app, ["-qq", "convert", str(self.test_file), str(output)])
+        assert logging.getLogger().level == logging.ERROR
+
+        self.runner.invoke(app, ["-vvvvv", "-qqqqq", "convert", str(self.test_file), str(output)])
+        assert logging.getLogger().level == logging.WARNING
