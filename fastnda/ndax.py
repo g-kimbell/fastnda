b3dd949e90d0960ae2631e27342f41959296844b
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-12-11 16:06:17 +0100

    Demote ndc log to debug

--- fastnda/ndax.py
+++ fastnda/ndax.py
@@ -213,7 +213,7 @@ def read_ndc(buf: bytes) -> pl.DataFrame:
     if reader is None:
         msg = f"ndc version {ndc_version} filetype {ndc_filetype} is not yet supported!"
         raise NotImplementedError(msg) from None
-    logger.info("Reading ndc version %d filetype %d", ndc_version, ndc_filetype)
+    logger.debug("Reading ndc version %d filetype %d", ndc_version, ndc_filetype)
     return reader(buf)
 
 
