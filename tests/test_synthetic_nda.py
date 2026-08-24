f7289afac6f028c6a46dec550736d880d4e23921
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-08-24 11:06:43 +0200

    Update nda10 synthetic test
    Time is in s not ms

--- tests/test_synthetic_nda.py
+++ tests/test_synthetic_nda.py
@@ -579,7 +579,7 @@ class TestReadNda10:
                 "index": [1, 2],
                 "step_index": [1, 2],
                 "step_type": [1, 2],
-                "step_time_s": [10000, 20000],
+                "step_time_s": [10, 20],
                 "voltage_V": [36000, 35000],
                 "current_mA": [200000, -150000],
                 "capacity_mAh": [1800000, 900000],
